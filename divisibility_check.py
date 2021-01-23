56e4b249efee4c12cbecdc76bbded5a0ebb258e6
merrickrowe <23mrowe@athenian.org>
2021-01-22 17:06:46 -0800

    Completed

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,3 +1,9 @@
 # Collaborators: 
-#
+for x in range (2, 51):
+    if int(x%3 == 0):
+        print (x)
+        print  ("x is divisible by 3")
+    else:
+        print (x)
+        print ("Not divisible by 3")
 
